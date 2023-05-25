fa31649f3f187af973e27f895695d70d4e95acf7
achtaitaipai <charles.cailleteau@gmail.com>
2023-05-25 17:18:07 +0200

    Add modulo operation support to library

--- string-math.js
+++ string-math.js
@@ -1,6 +1,6 @@
 function stringMath(eq, callback) {
   if (typeof eq !== 'string') return handleCallback(new TypeError('The [String] argument is expected.'), null);
-  const mulDiv = /([+-]?\d*\.?\d+(?:e[+-]\d+)?)\s*([*/])\s*([+-]?\d*\.?\d+(?:e[+-]\d+)?)/;
+  const mulDivMod = /([+-]?\d*\.?\d+(?:e[+-]\d+)?)\s*([*/%])\s*([+-]?\d*\.?\d+(?:e[+-]\d+)?)/;
   const plusMin = /([+-]?\d*\.?\d+(?:e[+-]\d+)?)\s*([+-])\s*([+-]?\d*\.?\d+(?:e[+-]\d+)?)/;
   const parentheses = /(\d)?\s*\(([^()]*)\)\s*/;
   var current;
@@ -14,21 +14,21 @@ function stringMath(eq, callback) {
   function fParentheses(eq) {
     while (eq.search(parentheses) !== -1) {
       eq = eq.replace(parentheses, function (a, b, c) {
-        c = fMulDiv(c);
+        c = fMulDivMod(c);
         c = fPlusMin(c);
         return typeof b === 'string' ? b + '*' + c : c;
       });
     }
-    eq = fMulDiv(eq);
+    eq = fMulDivMod(eq);
     eq = fPlusMin(eq);
     return eq;
   }
 
-  function fMulDiv(eq) {
-    while (eq.search(mulDiv) !== -1) {
-      eq = eq.replace(mulDiv, function (a) {
-        const sides = mulDiv.exec(a);
-        const result = sides[2] === '*' ? sides[1] * sides[3] : sides[1] / sides[3];
+  function fMulDivMod(eq) {
+    while (eq.search(mulDivMod) !== -1) {
+      eq = eq.replace(mulDivMod, function (a) {
+        const sides = mulDivMod.exec(a);
+        const result = sides[2] === '*' ? sides[1] * sides[3] : sides[2] === '/' ? sides[1] / sides[3] : sides[1] % sides [3];
         return result >= 0 ? '+' + result : result;
       });
     }
